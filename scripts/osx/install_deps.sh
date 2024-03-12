d0aa4512461d5f4c2c036b4ab865b33c950a644c
Sunil S <sunil.sarolkar.cds@gmail.com>
2024-03-12 06:56:04 +0530

    Updated to run on latest OSX

--- scripts/osx/install_deps.sh
+++ scripts/osx/install_deps.sh
@@ -14,6 +14,6 @@ done
 pip_packages="numpy<1.17 opencv-python<4.3"
 for pkg in $pip_packages
 do
-    echo "sudo -H python2 -m pip install $pkg"
-    sudo -H python2 -m pip install "$pkg"
+    echo "sudo -H python3 -m pip install $pkg"
+    sudo -H python3 -m pip install "$pkg"
 done
